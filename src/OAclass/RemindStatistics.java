b8a001b3d7c196339cfafe07600a88ad9ba4e33c
portekas <1610664728@qq.com>
2021-11-17 13:55:19 +0800

    更新

---
+++ src/OAclass/RemindStatistics.java
@@ -0,0 +1,58 @@
+package OAclass;
+
+import com.engine.kq.log.KQLog;
+import org.apache.commons.lang3.StringUtils;
+import weaver.conn.RecordSet;
+import weaver.interfaces.schedule.BaseCronJob;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @author lg
+ * @date 2021-10-25 08:47
+ */
+public class RemindStatistics extends BaseCronJob {
+    private KQLog kqLog = new KQLog();
+
+    public RemindStatistics(){
+
+    }
+
+    @Override
+    public void execute() {
+        this.kqLog.info("begin do RenewAccountingStatusJob invoke ...");
+        SimpleDateFormat sd = new SimpleDateFormat("yyyy-MM-dd");
+        Object[] arr;
+        RecordSet rec = new RecordSet();
+        RecordSet rec2 = new RecordSet();
+        rec.execute("select id,tjsql,txbt from uf_txxgb");
+        while (rec.next()){
+            int txsl = 0;
+            String sql = rec.getString("tjsql");
+            //解决参数中可能含有全角的问题
+            sql = sql.replaceAll("ｏｒ","or");
+            sql = sql.replaceAll("ａｎｄ","and");
+            sql = sql.replaceAll("ｉｎ","in");
+            sql = sql.replaceAll("ｓｅｌｅｃｔ","select");
+
+            if(StringUtils.isNotBlank(sql)){
+                rec2.execute(sql);
+                if(rec2.next()){
+                    txsl = rec2.getInt("txsl");
+                }
+            }
+            String txbt = rec.getString("txbt");
+            txbt = txbt.replace("$txsl$", String.valueOf(txsl));
+            int id = rec.getInt("id");
+            arr = new Object[3];
+            arr[0] = sd.format(new Date());
+            arr[1] = txsl;
+            arr[2] = txbt;
+            rec2.executeUpdate("update uf_txxgb set tjsj = ? , txsl = ? , txxsbt = ? where id = "+id,arr);
+        }
+        super.execute();
+    }
+}
