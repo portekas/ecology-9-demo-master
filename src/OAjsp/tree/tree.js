97d527748d685a24975657121748d85537114dc8
portekas <1610664728@qq.com>
2021-12-08 15:02:49 +0800

    更新

--- src/OAjsp/tree/tree.js
+++ src/OAjsp/tree/tree.js
@@ -3,7 +3,7 @@ $(function() {
         if($("#weleft").hasClass("ant-col-lg-5")){
             $("#weleft").attr("class","wea-left-right-layout-left ant-col-xs-0 ant-col-sm-0 ant-col-md-0 ant-col-lg-0");
         }else{
-            $("#weleft").attr("class","wea-left-right-layout-left ant-col-xs-8 ant-col-sm-7 ant-col-md-6 ant-col-lg-5");
+            $("#weleft").attr("class","wea-left-right-layout-left ant-col-xs-8 ant-col-sm-7 ant-col-md-6 ant-col-lg-5 weleft");
             $("#weleft").css({"overflow":"auto"});
         }
     })
