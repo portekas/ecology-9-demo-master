3a5640d901e23380f97bde23bd171fffc0862178
portekas <1610664728@qq.com>
2021-12-31 17:29:26 +0800

    更新

--- src/com/api/OAapi/RDPLoginApi.java
+++ src/com/api/OAapi/RDPLoginApi.java
@@ -1,11 +1,13 @@
 package com.api.OAapi;
 
 import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.lang.StringUtils;
 import weaver.conn.RecordSet;
 import weaver.general.Util;
 import weaver.hrm.HrmUserVarify;
 import weaver.hrm.User;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.GET;
@@ -22,7 +24,7 @@ public class RDPLoginApi {
      * 查询报表权限配置列表
      * 创建 2021-12-16 刘港
      * 修改 2021-12-21 刘港 返回参数中添加当前登录人ID、部门ID、相关部门ID、子部门ID
-     *
+     * 修改 2021-12-31 刘港 验证报表是否需要填写安全码 、 获取cookie信息验证是否填写过
      * @return
      */
     @GET
@@ -50,7 +52,6 @@ public class RDPLoginApi {
         }
         json.put("userxgbm",xgbmid);
 
-
         //查询子部门
         String zbm = "SELECT id FROM HrmDepartment hd WHERE hd.supdepid = '"+user.getUserDepartment()+"' AND (hd.canceled != 1 OR hd.canceled IS NULL)";
         xmbRec.execute(zbm);
@@ -65,7 +66,9 @@ public class RDPLoginApi {
         return json;
     }
 
-    //验证报表是否保密
+    /**
+     * 验证报表是否需要填写安全码
+     */
     @GET
     @Path("/rdpIsConfidential")
     @Produces(MediaType.APPLICATION_JSON)
@@ -76,10 +79,46 @@ public class RDPLoginApi {
         String bbid = Util.null2String(req.getParameter("bbid"));
         String xmbsql = "SELECT sfbmbb FROM uf_zhdybbqx uz LEFT JOIN uf_xxhmk uxk ON uxk.id = uz.bb WHERE uxk.bblj = '"+bbid+"' ";
         xmbRec.execute(xmbsql);
+        //需要填写
         if(xmbRec.next() && "1".equals(xmbRec.getString("sfbmbb"))){
-            isCon = "1";
+            //获取cookie信息验证是否填写过
+            boolean isHasKeyCok = getKeyCookie(req,res);
+            if(!isHasKeyCok){
+                isCon = "1";//需要填写验证
+            }
         }
         json.put("isCon",isCon);
         return json;
     }
+
+    /**
+     * 将报表安全码信息写入Cookie
+     */
+    @GET
+    @Path("/rdpAddKeyCookie")
+    @Produces(MediaType.APPLICATION_JSON)
+    public JSONObject addKeyCookie(@Context HttpServletRequest req, @Context HttpServletResponse res) {
+        JSONObject json = new JSONObject();
+        User user= HrmUserVarify.getUser(req,res);
+        json.put("userID",user.getUID());
+        return json;
+    }
+
+    /**
+     * 获取cookie信息验证是否填写过
+     * true 有填写过
+     * false 没填写过
+     */
+    public boolean getKeyCookie(@Context HttpServletRequest req, @Context HttpServletResponse res){
+        Cookie[] cookies = req.getCookies();
+        User user= HrmUserVarify.getUser(req,res);
+        String usCk = "UserKey_"+user.getUID();
+        for (Cookie cookie : cookies) {
+            if (usCk.equals(cookie.getName()) && StringUtils.isNotBlank(cookie.getValue())) {
+                return true;//有填写过
+            }
+        }
+        return false;//无填写过
+    }
+
 }
